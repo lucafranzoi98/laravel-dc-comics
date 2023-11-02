f72de38a4997d9b05aa8e95bed9a85ff1b9505b4
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-02 15:31:47 +0100

    migrate and seed

--- database/migrations/2023_11_02_141023_create_comics_table.php
+++ database/migrations/2023_11_02_141023_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->string('price', 10)->nullable();
+            $table->string('series', 50)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type', 50)->nullable();
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
             $table->timestamps();
         });
     }
